86a833027157992c6bcb42fe844f4243380a2363
abdalhababsah <abdalhababseh@outlook.com>
2025-01-22 16:23:12 +0300

    dashboard fix

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -2,13 +2,13 @@
 <html lang="en">
 
 <head>
-    @include('admin.partials.title-meta', ['title' => $title ?? 'Dashboard'])
+    @include('admin.partials.title-meta', ['title' => 'Dashboard'])
 
     <!-- Daterangepicker css -->
-    <link rel="stylesheet" href="{{ asset('assets/vendor/daterangepicker/daterangepicker.css') }}">
+    <link rel="stylesheet" href="{{asset('admin/assets/vendor/daterangepicker/daterangepicker.css')}}">
 
     <!-- Vector Map css -->
-    <link rel="stylesheet" href="{{ asset('assets/vendor/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css') }}">
+    <link rel="stylesheet" href="{{asset('admin/assets/vendor/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css')}}">
 
     @include('admin.partials.head-css')
 </head>
@@ -26,10 +26,10 @@
         <div class="content-page">
 
             @yield('content')
-
             @include('admin.partials.footer')
 
         </div>
+
         <!-- ============================================================== -->
         <!-- End Page content -->
         <!-- ============================================================== -->
@@ -42,21 +42,21 @@
     @include('admin.partials.footer-scripts')
 
     <!-- Daterangepicker js -->
-    <script src="{{ asset('assets/vendor/daterangepicker/moment.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/daterangepicker/daterangepicker.js') }}"></script>
+    <script src="{{asset('admin/assets/vendor/daterangepicker/moment.min.js')}}"></script>
+    <script src="{{asset('admin/assets/vendor/daterangepicker/daterangepicker.js')}}"></script>
 
     <!-- Apex Charts js -->
-    <script src="{{ asset('assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{asset('admin/assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
 
     <!-- Vector Map js -->
-    <script src="{{ asset('assets/vendor/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js') }}"></script>
+    <script src="{{asset('admin/assets/vendor/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
+    <script src="{{asset('admin/assets/vendor/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js')}}"></script>
 
     <!-- Dashboard App js -->
-    <script src="{{ asset('assets/js/pages/demo.dashboard.js') }}"></script>
+    <script src="{{asset('admin/assets/js/pages/demo.dashboard.js')}}"></script>
 
     <!-- App js -->
-    <script src="{{ asset('assets/js/app.min.js') }}"></script>
+    <script src="{{asset('admin/assets/js/app.min.js')}}"></script>
 
 </body>
 
