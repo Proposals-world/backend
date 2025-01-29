deff815fd14cc03482faff2a6cab21ca3e96d21d
abdalhababsah <abdalhababseh@outlook.com>
2025-01-29 19:50:03 +0300

    Refactor duplicate column definitions in hobbies table migration

--- database/migrations/2025_01_22_145541_create_hobbies_table.php
+++ database/migrations/2025_01_22_145541_create_hobbies_table.php
@@ -16,8 +16,6 @@ return new class extends Migration
             $table->id();
             $table->string('name_en');
             $table->string('name_ar');
-            $table->string('name_en');
-            $table->string('name_ar');
             $table->timestamps();
         });
     }
