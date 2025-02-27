70d0ac0b5db7666fffd2f517d630713bd5646a50
Samer Aljbour <samerseran34@example.com>
2025-02-27 17:06:47 +0300

    adjusted the filter now apply the prtef if no filter applied else apply filter

--- app/Http/Controllers/Api/FilterController.php
+++ app/Http/Controllers/Api/FilterController.php
@@ -17,14 +17,25 @@ class FilterController extends Controller
     {
         // Get the logged-in user's preferences
         $preferences = UserPreference::where('user_id', Auth::id())->first();
-
+        $isFromFilter = $request->input('isFilter', false);
         $query = UserProfile::with(['user', 'user.photos']);
         $query->whereHas('user', function ($query) use ($request) {
             $query->where('gender', '!=', Auth::user()->gender);
         });
+        // Filter out liked and disliked users
+        $likedUsers = Like::where('user_id', Auth::id())->pluck('liked_user_id');
+        $dislikedUsers = Dislike::where('user_id', Auth::id())->pluck('disliked_user_id');
 
+        $query->whereNotIn('id', $likedUsers);
+        $query->whereNotIn('id', $dislikedUsers);
+
+        // Gender preference (excluding users of the same gender as the logged-in user)
+        $query->whereHas('user', function ($query) use ($request) {
+            $query->where('gender', '!=', Auth::user()->gender);
+        });
         // Filter based on user's preferences if available
-        if ($preferences) {
+        if ($preferences && !$isFromFilter) {
+            // dd("from pref");
             // Nationality
             if ($preferences->preferred_nationality_id) {
                 $query->where('nationality_id', $preferences->preferred_nationality_id);
@@ -124,87 +135,80 @@ class FilterController extends Controller
             if ($preferences->preferred_marriage_budget_id) {
                 $query->where('marriage_budget_id', $preferences->preferred_marriage_budget_id);
             }
+        } else {
+            // Handle additional request filters (if any)
+            // dd("from filter");
+            if ($request->filled('nationality_id')) {
+                $query->where('nationality_id', $request->nationality_id);
+            }
+            if ($request->filled('origin_id')) {
+                $query->where('origin_id', $request->origin_id);
+            }
+            if ($request->filled('religion_id')) {
+                $query->where('religion_id', $request->religion_id);
+            }
+            if ($request->filled('religiosity_level_id')) {
+                $query->where('religiosity_level_id', $request->religiosity_level_id);
+            }
+            if ($request->filled('country_of_residence_id')) {
+                $query->where('country_of_residence_id', $request->country_of_residence_id);
+            }
+            if ($request->filled('city_id')) {
+                $query->where('city_id', $request->city_id);
+            }
+            if ($request->filled('age_min') && $request->filled('age_max')) {
+                $query->whereBetween('age', [$request->age_min, $request->age_max]);
+            }
+            if ($request->filled('educational_level_id')) {
+                $query->where('educational_level_id', $request->educational_level_id);
+            }
+            if ($request->filled('specialization_id')) {
+                $query->where('specialization_id', $request->specialization_id);
+            }
+            if ($request->filled('employment_status')) {
+                $query->where('employment_status', $request->employment_status);
+            }
+            if ($request->filled('job_title_id')) {
+                $query->where('job_title_id', $request->job_title_id);
+            }
+            if ($request->filled('financial_status_id')) {
+                $query->where('financial_status_id', $request->financial_status_id);
+            }
+            if ($request->filled('height_id')) {
+                $query->where('height_id', $request->height_id);
+            }
+            if ($request->filled('weight_id')) {
+                $query->where('weight_id', $request->weight_id);
+            }
+            if ($request->filled('marital_status_id')) {
+                $query->where('marital_status_id', $request->marital_status_id);
+            }
+            if ($request->filled('children')) {
+                $query->where('children', $request->children);
+            }
+            if ($request->filled('smoking_status')) {
+                $query->where('smoking_status', $request->smoking_status);
+            }
+            if ($request->filled('drinking_status_id')) {
+                $query->where('drinking_status_id', $request->drinking_status_id);
+            }
+            if ($request->filled('sports_activity_id')) {
+                $query->where('sports_activity_id', $request->sports_activity_id);
+            }
+            if ($request->filled('social_media_presence_id')) {
+                $query->where('social_media_presence_id', $request->social_media_presence_id);
+            }
+            if ($request->filled('sleep_habit_id')) {
+                $query->where('sleep_habit_id', $request->sleep_habit_id);
+            }
+            if ($request->filled('marriage_budget_id')) {
+                $query->where('marriage_budget_id', $request->marriage_budget_id);
+            }
         }
 
-        // Filter out liked and disliked users
-        $likedUsers = Like::where('user_id', Auth::id())->pluck('liked_user_id');
-        $dislikedUsers = Dislike::where('user_id', Auth::id())->pluck('disliked_user_id');
 
-        $query->whereNotIn('id', $likedUsers);
-        $query->whereNotIn('id', $dislikedUsers);
 
-        // Gender preference (excluding users of the same gender as the logged-in user)
-        $query->whereHas('user', function ($query) use ($request) {
-            $query->where('gender', '!=', Auth::user()->gender);
-        });
 
-        // Handle additional request filters (if any)
-        if ($request->filled('nationality_id')) {
-            $query->where('nationality_id', $request->nationality_id);
-        }
-        if ($request->filled('origin_id')) {
-            $query->where('origin_id', $request->origin_id);
-        }
-        if ($request->filled('religion_id')) {
-            $query->where('religion_id', $request->religion_id);
-        }
-        if ($request->filled('religiosity_level_id')) {
-            $query->where('religiosity_level_id', $request->religiosity_level_id);
-        }
-        if ($request->filled('country_of_residence_id')) {
-            $query->where('country_of_residence_id', $request->country_of_residence_id);
-        }
-        if ($request->filled('city_id')) {
-            $query->where('city_id', $request->city_id);
-        }
-        if ($request->filled('age_min') && $request->filled('age_max')) {
-            $query->whereBetween('age', [$request->age_min, $request->age_max]);
-        }
-        if ($request->filled('educational_level_id')) {
-            $query->where('educational_level_id', $request->educational_level_id);
-        }
-        if ($request->filled('specialization_id')) {
-            $query->where('specialization_id', $request->specialization_id);
-        }
-        if ($request->filled('employment_status')) {
-            $query->where('employment_status', $request->employment_status);
-        }
-        if ($request->filled('job_title_id')) {
-            $query->where('job_title_id', $request->job_title_id);
-        }
-        if ($request->filled('financial_status_id')) {
-            $query->where('financial_status_id', $request->financial_status_id);
-        }
-        if ($request->filled('height_id')) {
-            $query->where('height_id', $request->height_id);
-        }
-        if ($request->filled('weight_id')) {
-            $query->where('weight_id', $request->weight_id);
-        }
-        if ($request->filled('marital_status_id')) {
-            $query->where('marital_status_id', $request->marital_status_id);
-        }
-        if ($request->filled('children')) {
-            $query->where('children', $request->children);
-        }
-        if ($request->filled('smoking_status')) {
-            $query->where('smoking_status', $request->smoking_status);
-        }
-        if ($request->filled('drinking_status_id')) {
-            $query->where('drinking_status_id', $request->drinking_status_id);
-        }
-        if ($request->filled('sports_activity_id')) {
-            $query->where('sports_activity_id', $request->sports_activity_id);
-        }
-        if ($request->filled('social_media_presence_id')) {
-            $query->where('social_media_presence_id', $request->social_media_presence_id);
-        }
-        if ($request->filled('sleep_habit_id')) {
-            $query->where('sleep_habit_id', $request->sleep_habit_id);
-        }
-        if ($request->filled('marriage_budget_id')) {
-            $query->where('marriage_budget_id', $request->marriage_budget_id);
-        }
         // dd("filter");
         // Execute the query and return filtered users
         $users = $query->get();
