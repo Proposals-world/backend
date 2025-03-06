45897d6e654949e9fe7dbd8e16212e796761e154
SamerAljbour <samerseran34@gmail.com>
2025-03-06 10:34:55 +0300

    refactor the resource of the match

--- app/Http/Resources/MatchResource.php
+++ app/Http/Resources/MatchResource.php
@@ -26,10 +26,15 @@ class MatchResource extends JsonResource
 
         return [
             'id' => $this->id,
-            'user1_name' => $this->user1 ? $this->user1->first_name : null,
-            'user2_name' => $this->user2 ? $this->user2->first_name : null,
+            // 'user1_name' => $this->user1 ? $this->user1->first_name : null,
+            'matched_user_name' => $this->user2 ? $this->user2->first_name : null,
+            'matched_user_age' => $this->user1 && $this->user1->profile ? $this->user1->profile->age : null,
+            'matched_user_city' => $this->user1 && $this->user1->profile && $this->user1->profile->city
+                ? ($this->user1->profile->city->{'name_' . $lang} ?? null) : null,
             'match_status' => $this->match_status,
-            'contact_exchanged' =>  $this->contact_exchanged, // Convert to boolean
+            'contact_exchanged' => $this->contact_exchanged, // No need to convert, already a boolean
+            // 'user1_photo' => $this->user1 && $this->user1->photos()->first() ? $this->user1->photos()->first()->photo_url : null,
+            'matched_user_photo' => $this->user2 && $this->user2->photos()->first() ? $this->user2->photos()->first()->photo_url : null,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
