66772e32ed3b1a45ede16adff6fbc3a431618e41
Samer Aljbour <samerseran34@example.com>
2025-03-21 14:55:25 +0300

    update count of users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -110,6 +110,6 @@ class UserSeeder extends Seeder
                 'email_verified_at' => now(),
             ],
         ]);
-        User::factory(10)->create();
+        User::factory(1000)->create();
     }
 }
